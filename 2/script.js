fe12025d4baabd6a5a8ba5afbd0dfcf3a050c5ed
Shrikrishna Bhoi <112055594+shrikrishnabhoi@users.noreply.github.com>
2023-12-07 10:48:45 +0530

    Add files via upload

---
+++ 2/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+              strings: [ "SQL Developer", "Full Stack java Devloper"],
+              typeSpeed: 100,
+              backSpeed: 100,
+              backDelay: 1000,
+              loop: true
+})
+alert("it's just Demo Programe i'm not a working in any companies thank you");
